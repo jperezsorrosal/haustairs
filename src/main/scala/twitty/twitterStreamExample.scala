567602513a693fc5658b6234a2026cf0050ecfcf
Juan Pérez-Sorrosal <jperezsl@gmail.com>
2015-07-15 17:24:45 +0200

    Use Twitter Manager generated Credentials to access instead of implement 3-legged and oauth signing.

--- src/main/scala/twitty/twitterStreamExample.scala
+++ src/main/scala/twitty/twitterStreamExample.scala
@@ -17,31 +17,15 @@ object twitterStreamExample {
     val consumerToken = Token(conf.getString("houstairs.twitterConsumerKey"),
       conf.getString("houstairs.twitterConsumerSecret"))
 
-    val response = Http("https://api.twitter.com/oauth/request_token")
-      .postForm(Seq("oauth_callback" -> "oob"))
-      .oauth(consumerToken).asToken
+    val accessToken = Token(conf.getString("houstairs.twitterAccessToken"),
+      conf.getString("houstairs.twitterAccessTokenSecret"))
 
-    println(response.body)
-
-
-    println("To authorize this client app, go to https://api.twitter.com/oauth/authorize?oauth_token=" + response.body.key)
-
-    val verifier = Console.readLine("Enter verifier: ").trim
-    println(verifier)
-
-    val accessToken = Http("https://api.twitter.com/oauth/access_token")
-      .postForm
-      .oauth(consumerToken, response.body, verifier).asToken
-
-    println(Http("https://api.twitter.com/1.1/account/settings.json").oauth(consumerToken, accessToken.body).asString)
+    println(Http("https://api.twitter.com/1.1/account/settings.json").oauth(consumerToken, accessToken).asString)
 
     val request = Http("https://stream.twitter.com/1.1/statuses/filter.json")
       .param("track", "PlutoFlyby, scala, Grecia, Greece")
-      .oauth(consumerToken, accessToken.body)
-      .options(HttpOptions.connTimeout(500000), HttpOptions.readTimeout(500000))
-
-    //streaming api read it continuously and do whatever with it
-    //i am just printing it
+      .oauth(consumerToken, accessToken)
+      .options(HttpOptions.connTimeout(5000000), HttpOptions.readTimeout(5000000))
 
     request.execute(parser = { inputStream =>
       (Stream.continually(inputStream.read()).takeWhile(_ != -1)
